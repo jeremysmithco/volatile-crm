d9cf722c4dc16fc9dc3068311ac17b6ed9ba95cb
Jeremy Smith <jeremy@jeremysmith.co>
2025-08-29 13:40:47 -0400

    Run app:update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_234143) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_29_173934) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
